34123f232f6a58240c902a067483cf9d2eafc5a3
eescobedo <ezescobedo27@gmail.com>
2024-04-07 19:36:17 -0600

    Sql

---
+++ website.sql
@@ -0,0 +1,193 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: localhost
+-- Tiempo de generación: 08-04-2024 a las 01:35:31
+-- Versión del servidor: 8.0.17
+-- Versión de PHP: 7.3.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `website`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tbl_configuraciones`
+--
+
+CREATE TABLE `tbl_configuraciones` (
+  `ID` int(11) NOT NULL,
+  `nombreconfiguracion` varchar(255) NOT NULL,
+  `valor` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tbl_entradas`
+--
+
+CREATE TABLE `tbl_entradas` (
+  `ID` int(11) NOT NULL,
+  `fecha` varchar(255) NOT NULL,
+  `titulo` varchar(255) NOT NULL,
+  `descripcion` varchar(255) NOT NULL,
+  `imagen` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tbl_equipo`
+--
+
+CREATE TABLE `tbl_equipo` (
+  `ID` int(11) NOT NULL,
+  `imagen` varchar(255) NOT NULL,
+  `nombrecompleto` varchar(255) NOT NULL,
+  `puesto` varchar(255) NOT NULL,
+  `twiter` varchar(255) NOT NULL,
+  `facebook` varchar(255) NOT NULL,
+  `linkedin` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tbl_portafolio`
+--
+
+CREATE TABLE `tbl_portafolio` (
+  `ID` int(11) NOT NULL,
+  `titulo` varchar(255) NOT NULL,
+  `subtitulo` varchar(255) NOT NULL,
+  `imagen` varchar(255) NOT NULL,
+  `descripcion` varchar(255) NOT NULL,
+  `cliente` varchar(255) NOT NULL,
+  `categoria` varchar(255) NOT NULL,
+  `url` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tbl_servicios`
+--
+
+CREATE TABLE `tbl_servicios` (
+  `ID` int(11) NOT NULL,
+  `icono` varchar(255) NOT NULL,
+  `titulo` varchar(255) NOT NULL,
+  `descripcion` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tbl_usuarios`
+--
+
+CREATE TABLE `tbl_usuarios` (
+  `id` int(11) NOT NULL,
+  `usuario` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `correo` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `tbl_configuraciones`
+--
+ALTER TABLE `tbl_configuraciones`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- Indices de la tabla `tbl_entradas`
+--
+ALTER TABLE `tbl_entradas`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- Indices de la tabla `tbl_equipo`
+--
+ALTER TABLE `tbl_equipo`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- Indices de la tabla `tbl_portafolio`
+--
+ALTER TABLE `tbl_portafolio`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- Indices de la tabla `tbl_servicios`
+--
+ALTER TABLE `tbl_servicios`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- Indices de la tabla `tbl_usuarios`
+--
+ALTER TABLE `tbl_usuarios`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `tbl_configuraciones`
+--
+ALTER TABLE `tbl_configuraciones`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `tbl_entradas`
+--
+ALTER TABLE `tbl_entradas`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `tbl_equipo`
+--
+ALTER TABLE `tbl_equipo`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `tbl_portafolio`
+--
+ALTER TABLE `tbl_portafolio`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `tbl_servicios`
+--
+ALTER TABLE `tbl_servicios`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `tbl_usuarios`
+--
+ALTER TABLE `tbl_usuarios`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
