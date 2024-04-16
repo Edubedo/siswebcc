1854caf755e459cab3fa6ab77a151ac6c6c8de6c
eescobedo <ezescobedo27@gmail.com>
2024-04-16 04:32:38 -0600

    Posicionamiento

--- admin/index.php
+++ admin/index.php
@@ -7,7 +7,7 @@ if (!isset($_SESSION['usuario'])) {
 }
 include("./templates/header.php"); ?>
 
-<div class="container mt-5">
+<div class="container ">
     <div class="row">
         <div class="col-md-12">
          <h2>
@@ -22,9 +22,9 @@ include("./templates/header.php"); ?>
     </div>
 </div>
 
-<div class="p-5 mb-4 bg-light rounded-3 mt-5">
+<div class=" mb-4 bg-light rounded-3 mt-2">
     <div class="container-fluid py-5">
-        <h1 class="display-5 fw-bold">Sistemas web para el control de de</h1>
+        <h1 class="display-5 fw-bold">Sistema web para control de inventario y préstamos de material en centro de computo</h1>
         <p class="col-md-8 fs-4">
             Nuestro sistema de control de inventarios te permite gestionar y rastrear tus productos de manera eficiente. Puedes agregar, editar y eliminar productos, así como ver informes detallados de tus inventarios.
         </p>
