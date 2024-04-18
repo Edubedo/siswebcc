1361a5b01e2904c98a07874ab8f224de5f20c841
eescobedo <ezescobedo27@gmail.com>
2024-04-18 08:24:27 -0600

    Form

--- prestamos.php
+++ prestamos.php
@@ -74,61 +74,78 @@
     <!-- Service Start -->
     <div class="container-fluid pt-5">
         <div class="container">
-            <div class="section-title">
-                <h4 class="text-primary text-uppercase" style="letter-spacing: 5px;">Our Services</h4>
-                <h1 class="display-4">Fresh & Organic Beans</h1>
-            </div>
-            <div class="row">
-                <div class="col-lg-6 mb-5">
-                    <div class="row align-items-center">
-                        <div class="col-sm-5">
-                            <img class="img-fluid mb-3 mb-sm-0" src="img/service-1.jpg" alt="">
-                        </div>
-                        <div class="col-sm-7">
-                            <h4><i class="fa fa-truck service-icon"></i>Fastest Door Delivery</h4>
-                            <p class="m-0">Sit lorem ipsum et diam elitr est dolor sed duo. Guberg sea et et lorem dolor sed est sit
-                                invidunt, dolore tempor diam ipsum takima erat tempor</p>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-6 mb-5">
-                    <div class="row align-items-center">
-                        <div class="col-sm-5">
-                            <img class="img-fluid mb-3 mb-sm-0" src="img/service-2.jpg" alt="">
-                        </div>
-                        <div class="col-sm-7">
-                            <h4><i class="fa fa-coffee service-icon"></i>Fresh Coffee Beans</h4>
-                            <p class="m-0">Sit lorem ipsum et diam elitr est dolor sed duo. Guberg sea et et lorem dolor sed est sit
-                                invidunt, dolore tempor diam ipsum takima erat tempor</p>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-6 mb-5">
-                    <div class="row align-items-center">
-                        <div class="col-sm-5">
-                            <img class="img-fluid mb-3 mb-sm-0" src="img/service-3.jpg" alt="">
-                        </div>
-                        <div class="col-sm-7">
-                            <h4><i class="fa fa-award service-icon"></i>Best Quality Coffee</h4>
-                            <p class="m-0">Sit lorem ipsum et diam elitr est dolor sed duo. Guberg sea et et lorem dolor sed est sit
-                                invidunt, dolore tempor diam ipsum takima erat tempor</p>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-6 mb-5">
-                    <div class="row align-items-center">
-                        <div class="col-sm-5">
-                            <img class="img-fluid mb-3 mb-sm-0" src="img/service-4.jpg" alt="">
-                        </div>
-                        <div class="col-sm-7">
-                            <h4><i class="fa fa-table service-icon"></i>Online Table Booking</h4>
-                            <p class="m-0">Sit lorem ipsum et diam elitr est dolor sed duo. Guberg sea et et lorem dolor sed est sit
-                                invidunt, dolore tempor diam ipsum takima erat tempor</p>
-                        </div>
-                    </div>
-                </div>
-            </div>
+        <div class="card">
+    <div class="card-header">Crear prestamo</div>
+    <div class="card-body">
+    <form action="" enctype="multipart/form-data" method="post">
+        <div class="mb-3">
+            <label for="icono" class="form-label">Icono:</label>
+            <input
+                type="text"
+                class="form-control"
+                name="icono"
+                id="icono"
+                aria-describedby="helpId"
+                placeholder="Icono"
+                required="true"
+
+            />
+
+
+        </div> 
+
+        <div class="mb-3">
+            <label for="titulo" class="form-label">Título</label>
+            <input
+                type="text"
+                class="form-control"
+                name="titulo"
+                id="titulo"
+                aria-describedby="helpId"
+                placeholder="Título"
+                required="true"
+
+                />
         </div>
+        
+        <div class="mb-3">
+            <label for="descripcion" class="form-label">Descripción</label>
+            <input
+                type="text"
+                class="form-control"
+                name="descripcion"
+                id="descripcion"
+                aria-describedby="helpId"
+                placeholder="Descripción"
+                required="true"
+
+            />
+        </div>
+
+    <button
+        type="submit"
+        class="btn btn-success"
+    >
+        Agregar
+    </button>
+    <a
+        name=""
+        id=""
+        class="btn btn-danger"
+        href="index.php"
+        role="button"
+        >Volver</a
+    >
+    
+    
+    </form>
+    </div>
+    <div class="card-footer text-muted">
+
+    </div>
+</div>
+        </div>
+        </div> 
     </div>
     <!-- Service End -->
 
