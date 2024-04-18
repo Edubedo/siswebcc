346db72e5a5e06ae7de54bb6c0e5e373660b96bf
eescobedo <ezescobedo27@gmail.com>
2024-04-18 13:01:17 -0600

    Ahora si ya las sesiones

--- admin/bd.php
+++ admin/bd.php
@@ -1,16 +1,12 @@
-<?php 
-        $mes = require("./keys.php");
-
-    echo $keys['PASSWORD_DB'];
+<?php
+$keys = include("keys.php");
 $servidor = $keys['SERVER_DB']; // Nombre del servidor 
 $baseDeDatos = $keys['DB_DB']; // Nombre de la base de datos
-$usuario = $keys['USER_DB'];  // Nombre de usuario
+$usuario = $keys['USER_DB'];// Nombre de usuario 
 $contrasenia = $keys['PASSWORD_DB']; // Contraseña
 
-echo $servidor;
-
 try {
-    $conexion = new PDO("mysql:host=$servidor;dbname=$baseDeDatos",$usuario,$contrasenia);
+    $conexion = new PDO("mysql:host=$servidor;dbname=$baseDeDatos", $usuario, $contrasenia);
 } catch(Exception $error) {
     echo $error->getMessage();
 }
