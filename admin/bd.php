2c3dce32213e7032a4abe67bdf47734b0428d190
eescobedo <ezescobedo27@gmail.com>
2024-04-18 20:03:53 -0600

    Credenciales correctas

--- admin/bd.php
+++ admin/bd.php
@@ -1,19 +1,21 @@
 <?php
 
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 
+require_once __DIR__ . '/../vendor/autoload.php';
 
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/..');
+$dotenv->load();
 
-$keys = include("keys.php");
-
-$servidor = $keys['SERVER_DB']; // Nombre del servidor 
-$baseDeDatos = $keys['DB_DB']; // Nombre de la base de datos
-$usuario = $keys['USER_DB'];// Nombre de usuario 
-$contrasenia = $keys['PASSWORD_DB']; // Contraseña
-
+$servidor = $_ENV['SERVER_DB'];
+$baseDeDatos = $_ENV['DB_DB'];
+$usuario = $_ENV['USER_DB'];
+$contrasenia = $_ENV['PASSWORD_DB'];
 
 try {
     $conexion = new PDO("mysql:host=$servidor;dbname=$baseDeDatos", $usuario, $contrasenia);
 } catch(Exception $error) {
     echo $error->getMessage();
 }
-?>
