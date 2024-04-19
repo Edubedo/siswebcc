9e5ac0dc9b7833267ff5471e4f630d2f1ec7f65d
eescobedo <ezescobedo27@gmail.com>
2024-04-18 21:45:02 -0600

    Reconfig dev mode

--- admin/bd.php
+++ admin/bd.php
@@ -1,5 +1,15 @@
 <?php
 
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+// COMMENTS THIS LINES IN PROD MODE
+// require_once __DIR__ . '/../vendor/autoload.php';
+// $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/..');
+// $dotenv->load();
+// END COMMENTS
+
 $servidor = $_ENV['SERVER_DB'];
 $baseDeDatos = $_ENV['DB_DB'];
 $usuario = $_ENV['USER_DB'];
