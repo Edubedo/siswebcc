05d92982f4b5897a31cd84d0150b501292c292c0
eescobedo <ezescobedo27@gmail.com>
2024-04-20 07:11:46 -0600

    Arreglar imagen rota

--- index.php
+++ index.php
@@ -20,34 +20,8 @@
     <!-- Navbar Fin -->
 
     <!-- Carousel Inicio -->
-    <div class="p-0 mb-5 container-fluid">
-    <div id="blog-carousel" class="carousel slide overlay-bottom" data-ride="carousel">
-        <div class="carousel-inner">
-            <div class="carousel-item active">
-                <img class="w-100" src="img/udc.jpg" alt="Image">
-                <div class="carousel-caption d-flex flex-column align-items-center justify-content-center">
-                    <h2 class="m-0 text-white font-weight-medium">Sistema de control</h2>
-                    <h1 class="m-0 display-1 text-primary">CENTRO DE COMPUTO</h1>
-                    <h2 class="m-0 text-white">Universidad de Colima</h2>
-                </div>
-            </div>
-            <div class="carousel-item">
-                <img class="w-100" src="img/UDC5.jfif" alt="Image">
-                <div class="carousel-caption d-flex flex-column align-items-center justify-content-center">
-                    <h2 class="m-0 text-white font-weight-medium">Sistema de control</h2>
-                    <h1 class="m-0 display-1 text-primary">CENTRO DE COMPUTO</h1>
-                    <h2 class="m-0 text-white">Universidad de Colima</h2>
-                </div>
-            </div>
-        </div>
-        <a class="carousel-control-prev" href="#blog-carousel" data-slide="prev">
-            <span class="carousel-control-prev-icon"></span>
-        </a>
-        <a class="carousel-control-next" href="#blog-carousel" data-slide="next">
-            <span class="carousel-control-next-icon"></span>
-        </a>
-    </div>
-</div>
+    <?php include("./templates/components/carousel.php");?>
+
     <!-- Carousel Fin -->
   <!-- Servicios Inicio -->
   <?php include("./templates/views/servicios.php");?>
