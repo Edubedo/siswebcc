cb2ec53472b5b781c4e57abe8aec454a4e401fc2
eescobedo <ezescobedo27@gmail.com>
2024-04-20 00:05:54 -0600

    Resolver signo > escondido en el codigo

--- menu.php
+++ menu.php
@@ -16,7 +16,6 @@
 
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css" rel="stylesheet">
 
->
     <link href="lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet">
     <link href="lib/tempusdominus/css/tempusdominus-bootstrap-4.min.css" rel="stylesheet" />
 
