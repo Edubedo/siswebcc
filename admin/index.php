d99055e7184cf1fcbd923007612db258c9b29997
eescobedo <ezescobedo27@gmail.com>
2024-05-08 20:33:10 -0600

    Cambios

--- admin/index.php
+++ admin/index.php
@@ -10,12 +10,9 @@ include("./templates/header.php"); ?>
 
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
+
 <body>
     
 </body>
 </html>
+ 
